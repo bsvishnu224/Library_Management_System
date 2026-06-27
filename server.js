75c162b572434f81127417ec08bca97879580a37
bsvishnu224 <bsvvreddy@gmail.com>
2026-06-27 12:00:34 +0530

    Initial commit

--- server.js
+++ server.js
@@ -26,9 +26,12 @@ app.use(
   require("./routes/memberRoutes")
 );
 
+const PORT = process.env.PORT || 5000;
+
+
 app.listen(process.env.PORT, () => {
   console.log(
-    `Server running on port ${process.env.PORT}`
+    `Server running on port ${PORT}`
   );
 });
 
